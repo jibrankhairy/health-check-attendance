d25bd380eb85618f60c85f7f24c9c217c6178eb1
jibrankhairy <jibrankhairyy@gmail.com>
2025-08-15 10:39:58 +0700

    fix: error build vercel

--- app/api/mcu/reports/[reportId]/route.ts
+++ app/api/mcu/reports/[reportId]/route.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function GET(
-  request: Request,
-  { params }: { params: { reportId: string } }
+  _req: NextRequest,
+  { params }: { params: Promise<{ reportId: string }> }
 ) {
   try {
     const { reportId } = await params;
@@ -22,11 +22,7 @@ export async function GET(
       include: {
         patient: {
           include: {
-            company: {
-              select: {
-                name: true,
-              },
-            },
+            company: { select: { name: true } },
           },
         },
       },
